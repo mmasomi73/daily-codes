e94fe77a89b13f68b007a776ee31a9313e259f2a
mmasomi <mmasomi73@gmail.com>
2021-02-21 00:27:25 +0330

    instaDownloader

--- instaDownloader/downloader.py
+++ instaDownloader/downloader.py
@@ -1,8 +1,18 @@
 from datetime import datetime
+from itertools import takewhile, count
+
 from instaloader import *
-from itertools import dropwhile, takewhile
+
+def dateCondition(date, st, end):
+    # date = datetime.strptime(date, '%m/%d/%y %H:%M:%S')
+    st_date = datetime.strptime(st, '%Y-%m-%d %H:%M:%S')
+    end_date = datetime.strptime(end, '%Y-%m-%d %H:%M:%S')
+    return (st_date <= date) & (end_date >= date)
 
 
+# reminiscence10
+# sabrinaflowerphoto
+# 'chelseatea.r'
 i_username = 'xxxxxxx'
 i_passsword = 'xxxxxxx'
 
@@ -10,19 +20,21 @@ post_limiter = 3
 
 L = instaloader.Instaloader()
 L.login(i_username, i_passsword)
-
-mlist = ['chelseatea.r','aksgrafy', 'myflowergm', 'sabrinaflowerphoto', 'reminiscence10']
-
-SINCE = datetime(2021, 2, 18)
-UNTIL = datetime(2015, 2, 21)
+# aida = Post(L.context,'https://www.instagram.com/p/CLXBzLjgIjP/')
+mlist = ['chelseatea.r', 'aksgrafy', 'myflowergm', 'sabrinaflowerphoto', 'reminiscence10','kyoko29kyokolily']
+SINCE = '2021-02-18 0:0:0'
+UNTIL = '2021-02-21 0:0:0'
 
 for m in mlist:
     profile = Profile.from_username(L.context, m)
     i = 0
     posts = profile.get_posts()
-    for post in takewhile(lambda p: p.date > UNTIL, dropwhile(lambda p: p.date > SINCE, posts)):
+    # for post in posts:
+    #     print(post.date)
+    print('----------------= Working On {} Page'.format(m))
+    for post in takewhile(lambda p: dateCondition(p.date, SINCE, UNTIL), posts):
         # print(post.comments)
         L.download_post(post, target=profile.username)
         i += 1
-        if i >= post_limiter:
+        if i > post_limiter:
             break
