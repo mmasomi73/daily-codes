33176723048f7509cefee3352439cda72227a8c7
saber <marandisaber@gmail.com>
2021-12-11 05:36:27 +0330

    Update settings.py

--- GoogleCalendarLink/GoogleCalendarLink/settings.py
+++ GoogleCalendarLink/GoogleCalendarLink/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v@+heveg82q=_!r9!^2!*iu_w9+y%y$*f8^__y5kjm375x5z$)'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
